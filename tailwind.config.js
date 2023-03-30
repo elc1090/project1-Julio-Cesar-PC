2bc68c41825de104b8f88c4fd842dd79a94f42f1
Julio Cesar Polmann <juliocesarpolmann@gmail.com>
2023-03-30 13:56:22 -0300

    First

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: { 
+      extend: {
+        colors: {
+            primary: "#ACC12F",
+            secondary: "#96A13A",
+            tertiary: "#6C7D47",
+            quaternary: "#575C55",
+            quinary: "#45425A",
+            primaryHover: "#A1B135",
+
+        },
+    },
+    },
+    plugins: [],
+}
